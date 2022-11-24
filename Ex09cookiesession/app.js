5d0ef2814d4f8257dab0ce740d1382fe7e447c4f
나소연 <ssoy617@gmail.com>
2022-11-24 10:13:36 +0900

    221124 세션

--- Ex09cookiesession/app.js
+++ Ex09cookiesession/app.js
@@ -4,6 +4,7 @@ const cookieRouter = require("./routes/cookie");
 const sessionRouter = require("./routes/session");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const fileStore = require("session-file-store")(session);
 
 app.set("port", process.env.PORT || 8888);
 
@@ -13,6 +14,7 @@ app.use(
     httpOnly: true, //http 요청으로 온 것만 처리
     resave: false, //세션을 언제나 저장할지 설정
     secret: "secret key", //암호화 하는데 쓰일 키
+    store: new fileStore(), //여러 사용자들의 세션을 저장하기 위한 저장소
   })
 );
 
